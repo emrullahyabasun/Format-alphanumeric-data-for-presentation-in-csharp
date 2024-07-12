b4ad3cfaa7d1ff0e92ce71ca441175a0343611c4
emrullahyabasun <yabasunemrullah@gmail.com>
2024-07-12 13:51:34 +0300

    Interpolation

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
-﻿namespace Format_alphanumeric_data_for_presentation_in_csharp
+﻿using System.Xml.Linq;
+
+namespace Format_alphanumeric_data_for_presentation_in_csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //string first = "Hello";
+            //string second = "World";
+            //Console.WriteLine("{1} {0}!", first, second);
+            //Console.WriteLine("{0} {0} {0}!", first, second);
+
+            ////What is string interpolation ?
+            ////String interpolation is a technique that simplifies composite formatting.
+            ////Instead of using a numbered token and including the literal value or variable name in a list of arguments to String.Format() or Console.WriteLine(), you can just use the variable name inside of the curly braces.
+
             string first = "Hello";
             string second = "World";
-            string result = string.Format("{0} {1}!", first, second);
-            Console.WriteLine(result);
+            Console.WriteLine($"{first} {second}!");
+            Console.WriteLine($"{second} {first}!");
+            Console.WriteLine($"{first} {first} {first}!");
         }
     }
 }
