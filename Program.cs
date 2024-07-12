bd2c59bd951ed17624d1b052fc56e263bbfee514
emrullahyabasun <yabasunemrullah@gmail.com>
2024-07-12 13:49:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Format_alphanumeric_data_for_presentation_in_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "Hello";
+            string second = "World";
+            string result = string.Format("{0} {1}!", first, second);
+            Console.WriteLine(result);
+        }
+    }
+}
